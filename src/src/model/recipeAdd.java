2dd55085d0f6b249b39539d08614b56e49b550a1
dojo2022kudo <kudo-ae-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-14 11:55:31 +0900

    レシピJava(Beans),レシピ登録jsp更新

--- src/src/model/recipeAdd.java
+++ src/src/model/recipeAdd.java
@@ -1,30 +1,82 @@
 package model;
 
-import java.io.IOException;
+import java.io.Serializable;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+public class recipeAdd implements Serializable{
+	private int id;
+	private int userid;
+	private String recipe;
+	private int cost;
+	private int time;
+	private String url;
+	private String remarks;
 
-public class recipeAdd extends HttpServlet{
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(String recipe) {
+		this.recipe = recipe;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getRemarks() {
+		return remarks;
+	}
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	//コンストラクタ
+	public recipeAdd(int id, int userid, String recipe, int cost, int time, String url, String remarks) {
+		super();
+		this.id = id;
+		this.userid = userid;
+		this.recipe = recipe;
+		this.cost = cost;
+		this.time = time;
+		this.url = url;
+		this.remarks = remarks;
 	}
+
+	//引数のないコンストラクタ
+		public recipeAdd() {
+			this.id = 0;
+			this.userid = 0;
+			this.recipe = "";
+			this.cost = 0;
+			this.time = 0;
+			this.url = "";
+			this.remarks = "";
+		}
+
 
 }
 
