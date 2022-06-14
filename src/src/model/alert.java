1a699a061906bb7b2f143485543ec7b5c1d07c27
dojo2022-ochiai <103226473+dojo2022-ochiai@users.noreply.github.com>
2022-06-14 11:28:31 +0900

    alertのBeans完成

--- src/src/model/alert.java
+++ src/src/model/alert.java
@@ -1,33 +1,100 @@
 package model;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.Serializable;
 
-/**
- * Servlet implementation class alert
- */
-@WebServlet("/alert")
-public class alert extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class alert implements Serializable{
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	private int id;
+	private int userid;
+	private String days;
+	private int morning_min;
+	private int lunch_min;
+	private int dinner_min;
+	private int morning_max;
+	private int lunch_max;
+	private int dinner_max;
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getDays() {
+		return days;
+	}
+	public void setDays(String days) {
+		this.days = days;
+	}
+	public int getMorning_min() {
+		return morning_min;
+	}
+	public void setMorning_min(int morning_min) {
+		this.morning_min = morning_min;
+	}
+	public int getLunch_min() {
+		return lunch_min;
+	}
+	public void setLunch_min(int lunch_min) {
+		this.lunch_min = lunch_min;
+	}
+	public int getDinner_min() {
+		return dinner_min;
+	}
+	public void setDinner_min(int dinner_min) {
+		this.dinner_min = dinner_min;
+	}
+	public int getMorning_max() {
+		return morning_max;
+	}
+	public void setMorning_max(int morning_max) {
+		this.morning_max = morning_max;
+	}
+	public int getLunch_max() {
+		return lunch_max;
+	}
+	public void setLunch_max(int lunch_max) {
+		this.lunch_max = lunch_max;
+	}
+	public int getDinner_max() {
+		return dinner_max;
+	}
+	public void setDinner_max(int dinner_max) {
+		this.dinner_max = dinner_max;
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+	public alert(int id, int userid, String days, int morning_min, int lunch_min, int dinner_min, int morning_max,
+			int lunch_max, int dinner_max) {
+		super();
+		this.id = id;
+		this.userid = userid;
+		this.days = days;
+		this.morning_min = morning_min;
+		this.lunch_min = lunch_min;
+		this.dinner_min = dinner_min;
+		this.morning_max = morning_max;
+		this.lunch_max = lunch_max;
+		this.dinner_max = dinner_max;
 	}
 
+
+	public alert() {
+		this.id = 0;
+		this.userid = 0;
+		this.days = "";
+		this.morning_min = 0;
+		this.lunch_min = 0;
+		this.dinner_min = 0;
+		this.morning_max = 0;
+		this.lunch_max = 0;
+		this.dinner_max = 0;
+	}
 }
