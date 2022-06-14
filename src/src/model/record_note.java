cad85107131e003e2ac79760f603a99338ad7ede
dojo2022-ochiai <103226473+dojo2022-ochiai@users.noreply.github.com>
2022-06-14 14:54:58 +0900

    記録備考修正

--- src/src/model/record_note.java
+++ src/src/model/record_note.java
@@ -1,63 +1,63 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 	public class record_note implements Serializable {
 		//テーブルの項目を再現
-		 private String recipe;
-		private int cost;
-		private int time;
-		private String url;
-		private String notes;
-		public String getRecipe() {
-			return recipe;
+		private int id;
+		private int userid;
+		private Date date;
+		private String remarks;
+
+		//getter, setter
+		public int getId() {
+			return id;
 		}
 
+		public void setId(int id) {
+			this.id = id;
+		}
 
-		public void setRecipe(String recipe) {
-			this.recipe = recipe;
+		public int getUserid() {
+			return userid;
 		}
-		public int getCost() {
-			return cost;
+
+		public void setUserid(int userid) {
+			this.userid = userid;
 		}
-		public void setCost(int cost) {
-			this.cost = cost;
+
+		public Date getDate() {
+			return date;
 		}
-		public int getTime() {
-			return time;
+
+		public void setDate(Date date) {
+			this.date = date;
 		}
-		public void setTime(int time) {
-			this.time = time;
+
+		public String getRemarks() {
+			return remarks;
 		}
-		public String getUrl() {
-			return url;
+
+		public void setRemarks(String remarks) {
+			this.remarks = remarks;
 		}
-		public void setUrl(String url) {
-			this.url = url;
-		}
-		public String getNotes() {
-			return notes;
-		}
-		public void setNotes(String notes) {
-			this.notes = notes;
-		}
-		public record_note(String recipe, int cost, int time, String url, String notes) {
+
+		//コンストラクタ
+		public record_note(int id, int userid, Date date, String remarks) {
 			super();
-			this.recipe = recipe;
-			this.cost = cost;
-			this.time = time;
-			this.url = url;
-			this.notes = notes;
+			this.id = id;
+			this.userid = userid;
+			this.date = date;
+			this.remarks = remarks;
 		}
 
+		//デフォルトコンストラクタ
 		public record_note() {
-			this.recipe = "";
-			this.cost = 0;
-			this.time = 0;
-			this.url = "";
-			this.notes = "";
+			this.id = 0;
+			this.userid = 0;
+			this.date = null;
+			this.remarks = "";
 		}
-
-
 
 }
