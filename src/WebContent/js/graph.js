6c6775509535a2b711d39ae3b0b29a3cc94d6b2f
dojo2022-ochiai <103226473+dojo2022-ochiai@users.noreply.github.com>
2022-06-16 17:15:28 +0900

    グラフの生成処理追加

--- src/WebContent/js/graph.js
+++ src/WebContent/js/graph.js
@@ -1,3 +1,28 @@
-/**
- * 
- */
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js" integrity="sha512-sW/w8s4RWTdFFSduOTGtk4isV1+190E/GghVffMA9XczdJ2MDzSzLEubKAs5h0wzgSJOQTRYyaz73L3d6RtJSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script>
+ var ctx = document.getElementById('myChart').getContext('2d');
+ var chart = new Chart(ctx, {
+     type: 'line',
+
+     // データを指定
+     data: {
+         labels: ['月', '火', '水', '木', '金', '土', '日'],
+         datasets: [{
+             label: 'dataset example',
+             borderColor: 'rgb(75, 192, 192)',
+             fill: false,
+             data: [10, 2, 5, 4, 6, 7, 11]
+         }]
+     },
+
+     // 設定はoptionsに記述
+     options: {
+       //タイトル
+       title: {
+         display: true,
+         text: '線グラフの例'
+       }
+     }
+});
+</script>
