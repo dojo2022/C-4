7eb714428e0bc4bad216d74412fb3ef82b64592e
dojo2022-ochiai <103226473+dojo2022-ochiai@users.noreply.github.com>
2022-06-20 10:20:12 +0900

    未入力時の処理追加

--- src/WebContent/js/newPw.js
+++ src/WebContent/js/newPw.js
@@ -1,3 +1,20 @@
-/**
- * 
- */
+function chafun(){
+	document.getElementById('chaform').onsubmit = function(){
+		//各データを取得
+    	const oldpw=document.getElementById('chaform').oldpw.value;
+    	const newpw=document.getElementById('chaform').newpw.value;
+
+		//未入力欄の検出とエラーメッセージの表示
+		if(newpw === ''){
+			document.getElementById('err').textContent='新しいパスワードを入力してください。';
+			return false;
+    	} else if(oldpw === newpw){
+			document.getElementById('err').textContent='既存のパスワードとは異なるものを入力してください。';
+			return false;
+		} else {
+			//全メッセージを削除
+			document.getElementById('err').textContent='';
+		}
+
+ 	};
+};
