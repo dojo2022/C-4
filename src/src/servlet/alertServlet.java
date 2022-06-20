faeed18f98dda7751490207ca4a995e76bba6bb8
dojo2022-ochiai <103226473+dojo2022-ochiai@users.noreply.github.com>
2022-06-20 10:19:02 +0900

    処理内容の変更

--- src/src/servlet/alertServlet.java
+++ src/src/servlet/alertServlet.java
@@ -42,31 +42,51 @@ public class alertServlet extends HttpServlet {
 		//各nameから情報を取る
 		user user = (user) session.getAttribute("allList");
 		int userid = user.getId();
-		String days = request.getParameter("days");
+		String[] days = new String[7];
+		days[0] = request.getParameter("Mon");
+		days[1] = request.getParameter("Tue");
+		days[2] = request.getParameter("Wed");
+		days[3] = request.getParameter("Thu");
+		days[4] = request.getParameter("Fri");
+		days[5] = request.getParameter("Sat");
+		days[6] = request.getParameter("Sun");
 		int morning_min = Integer.parseInt(request.getParameter("morning_min"));
 		int lunch_min = Integer.parseInt(request.getParameter("lunch_min"));
 		int dinner_min = Integer.parseInt(request.getParameter("dinner_min"));
 		int morning_max = Integer.parseInt(request.getParameter("morning_max"));
 		int lunch_max = Integer.parseInt(request.getParameter("lunch_max"));
 		int dinner_max = Integer.parseInt(request.getParameter("dinner_max"));
-
+		int count = 0;
+		int err = 0;
 		//るーぷ
 
 		// 更新を行う
 
 		alertDAO bDao = new alertDAO();
 		if (request.getParameter("SUBMIT").equals("確定")) {
-			if (bDao.update(new alert(0, userid, days, morning_min, lunch_min, dinner_min, morning_max, lunch_max,
-					dinner_max))) {
-				request.setAttribute("result",
-						new result("", "", "/EngelS/homeServlet"));
-			}
-			else { // 更新失敗
-			request.setAttribute("result",
-					new result("", "", ""));
-					// 結果ページにフォワードする
+			for(int i = 0; i < 7; i++) {
+				if(days[i] != null) {
+					if (bDao.update(new alert(0, userid, days[i], morning_min, lunch_min, dinner_min, morning_max, lunch_max,
+							dinner_max))) {
+						count++;
+					} else if (bDao.insert(new alert(0, userid, days[i], morning_min, lunch_min, dinner_min, morning_max, lunch_max,
+							dinner_max))){
+						count++;
+					}else {
+						err++;
+					}
+				}
 			}
 
+			if(err > 0) {
+				session.setAttribute("result",new result(count + err + "つ中、" + err + "つのアラート設定に失敗しました。", "", ""));
+			} else {
+				session.setAttribute("result",new result(count + "つのアラート設定を行いました。", "", ""));
+			}
+			response.sendRedirect("/EngelS/alertServlet");
 		}
+		//request.setAttribute("result",new result("", "", ""));
+		// 結果ページにフォワードする
+
 	}
 }
