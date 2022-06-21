ed349a34a6bfe4d4998537e71137794c3ba95a51
dojo2022-ochiai <103226473+dojo2022-ochiai@users.noreply.github.com>
2022-06-21 09:19:41 +0900

    グラフ生成処理の追加

--- src/WebContent/js/graph.js
+++ src/WebContent/js/graph.js
@@ -1,28 +1,84 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js" integrity="sha512-sW/w8s4RWTdFFSduOTGtk4isV1+190E/GghVffMA9XczdJ2MDzSzLEubKAs5h0wzgSJOQTRYyaz73L3d6RtJSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+	type : 'line',
 
-<script>
- var ctx = document.getElementById('myChart').getContext('2d');
- var chart = new Chart(ctx, {
-     type: 'line',
+	// データを指定
+	data : {
+		labels : Arraylabel(),
+		datasets : [ {
+			label : '1日の削減金額',
+			borderColor : 'rgb(75, 192, 192)',
+			fill : false,
+			data : Arraydata()
 
-     // データを指定
-     data: {
-         labels: ['月', '火', '水', '木', '金', '土', '日'],
-         datasets: [{
-             label: 'dataset example',
-             borderColor: 'rgb(75, 192, 192)',
-             fill: false,
-             data: [10, 2, 5, 4, 6, 7, 11]
-         }]
-     },
+		}, {
+			label : '1日のノルマ',
+			borderColor : '#f88',
+			fill : false,
+			data : Arrayquota()
+		} ]
+	},
 
-     // 設定はoptionsに記述
-     options: {
-       //タイトル
-       title: {
-         display: true,
-         text: '線グラフの例'
-       }
-     }
+	// 設定はoptionsに記述
+	options : {
+		//タイトル
+		title : {
+			display : true,
+			text : '日々の削減金額'
+		},
+		//y軸の最小値を設定,
+		y: {
+			min: 0,
+		},
+		scales: {
+			xAxes: [{
+				scaleLabel: {
+					display: true,
+					labelString: '日付'
+				}
+			}],
+			yAxes: [{
+				scaleLabel: {
+					display: true,
+					labelString: '金額：円'
+				}
+			}]
+		}
+	}
 });
-</script>
+
+//グラフデータの設定
+function Arraydata(){
+	var hoge = document.getElementsByClassName('savings');
+	var data = [];
+
+	for(let i = 0; i < hoge.length; i++){
+		data.push(hoge[i].textContent);
+	};
+	return data;
+}
+
+//ラベルの設定
+function Arraylabel(){
+	var hoge = document.getElementsByClassName('date');
+	var label = [];
+
+	for(let i = 0; i < hoge.length; i++){
+		label.push(hoge[i].textContent);
+	};
+	return label;
+}
+
+//目標金額の日割り計算
+function Arrayquota(){
+	var hoge = document.getElementsByClassName('date');
+	const goal = document.getElementsByClassName('goal');
+	const daymax = document.getElementsByClassName('daymax');
+	const quotadata = Math.floor(goal[0].textContent/daymax[0].textContent);
+	var quota = [];
+
+	for(let i = 0; i < hoge.length; i++){
+		quota.push(quotadata);
+	};
+	return quota;
+}
