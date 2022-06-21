07c14cf59b4c1038df414251fd350208b6e742c8
dojo2022yamaguchiyoshiki <103239448+dojo2022yamaguchiyoshiki@users.noreply.github.com>
2022-06-21 18:48:01 +0900

    jsファイルです

--- src/WebContent/js/records.js
+++ src/WebContent/js/records.js
@@ -1,12 +1,15 @@
 'use strict';
 
 function plus() {
-	document.getElementById('number1');
-	document.getElementById('number2');
-	document.getElementById('number3');
+	let number1 = document.getElementById('number1');
+	let number2 = document.getElementById('number2');
+	let number3 = document.getElementById('number3');
+
+	console.log(number1);
+	console.log(number2);
+	console.log(number3);
 	//結果を戻す
-	//const totals = number1 + number2 + number3;
-	const total_value = document.getElementById('total').value = number1 + number2 + number3;
+	document.getElementById('total').textContent = number1 + number2 + number3;
 }
 
 //必須項目を入力してくださいとだけを出るようにする。 required=""で実装してみる
@@ -39,17 +42,97 @@ function records() {
 			return false;
 		}
 	}
+}
 
-	//朝食１品追加
-	function addMorningMenu() {
 
-		//どこに足すか
-		let td = document.getElementById('morning');
+//朝食１品追加
+function addMorningMenu() {
 
-		//何を足すか
-		let select = document.getElementById('morningSelect').cloneNode(true);
-		td.appendChild(document.createElement('br'));
-		td.appendChild(select);
+	//レシピのプルダウンを追加する処理
+	//①どこに足すか
+	let td = document.getElementById('morning_recipe');
 
-	}
+	//②何を足すか
+	let select = document.getElementById('morningRecipe').cloneNode(true);
+
+	//select変数のonchangeイベントにselectFunctionを紐づける。
+	select.addEventListener('onchange', selectFunction, false);
+
+	//brを入れて次に段になるようしている。
+	td.appendChild(document.createElement('br'));
+
+	//①で取得したelement要素に、②の要素を追加
+	td.appendChild(select);
+}
+
+
+//昼食１品追加
+function addlunchMenu() {
+
+	//レシピのプルダウンを追加する処理
+	//①どこに足すか
+	let td = document.getElementById('lunch_recipe');
+
+	//②何を足すか
+	let select = document.getElementById('lunchRecipe').cloneNode(true);
+
+	//brを入れて次に段になるようしている。
+	td.appendChild(document.createElement('br'));
+
+	//①で取得したelement要素に、②の要素を追加
+	td.appendChild(select);
+}
+
+
+//夕食1品追加
+function adddinnerMenu() {
+
+	//レシピのプルダウンを追加する処理
+	//①どこに足すか
+	let td = document.getElementById('dinner_recipe');
+
+	//②何を足すか
+	let select = document.getElementById('dinnerRecipe').cloneNode(true);
+
+	//brを入れて次に段になるようしている。
+	td.appendChild(document.createElement('br'));
+
+	//①で取得したelement要素に、②の要素を追加
+	td.appendChild(select);
+}
+
+//レシピと金額連動させる 朝
+function selectFunction(){
+	//
+	const select = document.getElementById('morning');
+	const savings = document.getElementById('morning_savings');
+	let index = select.selectedIndex;
+	//console.log(index);
+
+	//選ぶ
+	savings.selectedIndex = index;
+}
+
+//レシピと金額連動させる 昼
+function selectFunctions(){
+	//
+	const select = document.getElementById('lunch');
+	const savings = document.getElementById('lunch_savings');
+	let index = select.selectedIndex;
+	//console.log(index);
+
+	//選ぶ
+	savings.selectedIndex = index;
+}
+
+//レシピと金額連動させる 夕
+function selectFunctionss(){
+	//
+	const select = document.getElementById('dinner');
+	const savings = document.getElementById('dinner_savings');
+	let index = select.selectedIndex;
+	//console.log(index);
+
+	//選ぶ
+	savings.selectedIndex = index;
 }
