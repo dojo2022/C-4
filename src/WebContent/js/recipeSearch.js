5259c22132d5593131119868369ed801a4e1ebc6
dojo2022-meguro <103238169+dojo2022-meguro@users.noreply.github.com>
2022-06-23 16:11:54 +0900

    アレンジを加えただけで、動き的にはあまり変わっていません

--- src/WebContent/js/recipeSearch.js
+++ src/WebContent/js/recipeSearch.js
@@ -51,31 +51,29 @@ const overlay = document.querySelector('.overlay');
 
 
 function onclick_regist(){
-	//モーダルウィンドウの表示したい場所に画面の値をセットする。
-	//☆js文51行目、コスト以降合っているか要確認
-	//innerHTML：HTML要素の取得、変更、削除、追加などが可能。
 
-	//レシピ名recipeの分
+
+	//レシピ名recipe
 	let recipe = document.getElementById("recipe").value;
 	document.getElementById("modal_recipe").innerHTML = recipe;
 	document.getElementById("hidden_recipe").value = recipe;
 
-	//費用costの分
+	//費用cost
 	let cost = document.getElementById("cost").value;
 	document.getElementById("modal_cost").innerHTML = cost;
 	document.getElementById("hidden_cost").value = cost;
 
-	//所要時間timeの分
+	//所要時間time
 	let time = document.getElementById("time").value;
 	document.getElementById("modal_time").innerHTML = time;
 	document.getElementById("hidden_time").value = time;
 
-	//参考URLurlの分
+	//参考URLurl
 	let url = document.getElementById("url").value;
 	document.getElementById("modal_url").innerHTML = url;
 	document.getElementById("hidden_url").value = url;
 
-	//備考remarksの分
+	//備考remarks
 	let remarks = document.getElementById("remarks").value;
 	document.getElementById("modal_remarks").innerHTML = remarks;
 	document.getElementById("hidden_remarks").value = remarks;
