fed43067f9cd5a66eaa398ef24a81b44026f5240
dojo2022-ochiai <103226473+dojo2022-ochiai@users.noreply.github.com>
2022-06-23 13:46:55 +0900

    アラートでのレシピ検索機能を追加

--- src/src/dao/recipeDAO.java
+++ src/src/dao/recipeDAO.java
@@ -202,4 +202,68 @@ public class recipeDAO {
 		}//13行目 public boolean～の閉じ
 
 
+		//アラートで使用するレシピ検索
+		public List<recipeAdd> randomSelect(recipeAdd randomRecipe) {
+			Connection conn = null;
+			List<recipeAdd> sample = new ArrayList<recipeAdd>();
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6_data/C4", "sa", "");
+
+				// SQL文を準備する
+				String sql = "select recipe from recipe where (userid = 0 or userid = ?) and time <= ? and recipe != '外食';";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる 改造ポイント
+				pStmt.setInt(1, randomRecipe.getUserid() );
+				pStmt.setInt(2, randomRecipe.getTime() );
+
+				// SQL文を実行し、rsという結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				//rsから結果を取り出す
+				while(rs.next()) {
+					recipeAdd RL= new recipeAdd(
+							0,
+							0,
+							rs.getString("recipe"),
+							0,
+							0,
+							"",
+							""
+					);
+					sample.add(RL);
+				}
+
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				sample = null;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				sample = null;
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						sample = null;
+					}
+				}
+			}
+			// 結果を返す
+			return sample;
+		}
+
+
+
 }//10行目DAOの閉じ
